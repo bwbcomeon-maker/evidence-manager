2687a5e44793c930ebf0b0cfc1aba168d99e0d84
bwbcomeon-maker <bwbcomeon@gmail.com>
2026-01-26 14:37:48 +0800

    commit spring-boot

---
+++ backend/app/src/test/java/com/bwbcomeon/evidence/EvidenceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwbcomeon.evidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvidenceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
